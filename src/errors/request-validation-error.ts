3d2cb706ff7346b18d7ae4957789c709d62e4bbc
Joseph Reed <jereed559@gmail.com>
2020-08-22 22:15:47 -0700

    Updates

--- src/errors/request-validation-error.ts
+++ src/errors/request-validation-error.ts
@@ -1,11 +1,19 @@
-import { ValidationError } from 'express-validator';
-import { CustomError } from './custom-error';
-export declare class RequestValidationError extends CustomError {
-    statusCode: number;
-    errors: ValidationError[];
-    constructor(errors: ValidationError[]);
-    serializeErrors(): {
-        message: any;
-        field: string;
-    }[];
+import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
+
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+
+  constructor(public errors: ValidationError[]) {
+    super("Invalid request parameters");
+
+    // Only because we are extending a built in class
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((err) => {
+      return { message: err.msg, field: err.param };
+    });
+  }
 }
